d1b729f6b7036b3d2c44d73a3999fab0586f0102
Renan Fernandes <renzinestuods@gmail.com>
2022-08-26 17:14:11 -0300

    Cria o reducer wallet

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
+};
+
+export default function wallet(state = INITIAL_STATE, { type, payload }) {
+  switch (type) {
+  case 'WALLET': return {
+    ...state,
+    ...payload,
+  };
+  default: return state;
+  }
+}
