d8895ce7fa38c4ab1ff95d8f2cd3c41fb4822a8b
Renan Fernandes <renzinestuods@gmail.com>
2022-08-26 17:09:58 -0300

    Cria o reducer user

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+export default function user(state = INITIAL_STATE, { type, payload }) {
+  switch (type) {
+  case 'USER': return {
+    ...state,
+    ...payload,
+  };
+  default: return state;
+  }
+}
