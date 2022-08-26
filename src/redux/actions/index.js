25aa14338f5aeb3f1a41b53cb8fdb9f03bb5ca40
Renan Fernandes <renzinestuods@gmail.com>
2022-08-26 20:52:08 -0300

    Apaga a action responseAction. Chama a walleAction no lugar da responseAction

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,4 @@
 export const REQUEST_API = 'REQUEST_API';
-export const RESPONSE = 'RESPONSE';
 export const USER = 'USER';
 export const WALLET = 'WALLET';
 
@@ -9,13 +8,11 @@ export const walletAction = (payload) => ({ type: WALLET, payload });
 
 const requestAction = () => ({ type: REQUEST_API });
 
-const responseAction = (payload) => ({ type: RESPONSE, payload });
-
 export default function fetchAPI() {
   return async (describe) => {
     describe(requestAction());
     const response = await fetch('https://economia.awesomeapi.com.br/json/all');
     const data = await response.json();
-    describe(responseAction(data));
+    describe(walletAction(data));
   };
 }
