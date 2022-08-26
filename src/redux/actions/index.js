8b4919e2779712af9a04a2d42ae277f16ce2f658
Renan Fernandes <renzinestuods@gmail.com>
2022-08-26 17:58:31 -0300

    Cria a resquestAction

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -6,3 +6,5 @@ export const WALLET = 'WALLET';
 export const userAction = (payload) => ({ type: USER, payload });
 
 export const walletAction = (payload) => ({ type: WALLET, payload });
+
+const requestAction = () => ({ type: REQUEST_API });
