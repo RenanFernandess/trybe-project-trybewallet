d297e8348ec1cfb6a1ad635b7ba5116bf1e8034e
Renan Fernandes <renzinestuods@gmail.com>
2022-08-26 21:17:40 -0300

    Remover o import do reducer exchang

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import user from './user';
 import wallet from './wallet';
-import exchange from './exchange';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
@@ -9,7 +8,6 @@ import exchange from './exchange';
 const rootReducer = combineReducers({
   user,
   wallet,
-  exchange,
 });
 
 export default rootReducer;
