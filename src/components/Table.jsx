7b2a5d92aaf051fd5fda7db579c378de5b794f97
Renan Fernandes <renzinestuods@gmail.com>
2022-08-29 19:23:32 -0300

    REQUISITO-7: Implementa a a lógica para que a tabela seja alimentada pelo estado da aplicação

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Table.css';
+import propTypes from 'prop-types';
 
 class Table extends Component {
+  formatNumber(value) {
+    const regExp = /^(\d+\.\d{2})/gm;
+    if (!regExp.test(value)) return `${value}.00`;
+    return `${value}`.match(regExp);
+  }
+
   render() {
+    const { expenses } = this.props;
+
     return (
       <table className="table">
         <thead className="thead">
@@ -19,10 +28,41 @@ class Table extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody className="tbody">
+          {
+            expenses.length
+              ? expenses
+                .map((
+                  { value, description, currency, method, tag, exchangeRates },
+                  index,
+                ) => {
+                  const { [currency]: { name, ask } } = exchangeRates;
+
+                  return (
+                    <tr key={ index }>
+                      <td>{ description }</td>
+                      <td>{ tag }</td>
+                      <td>{ method }</td>
+                      <td>{ this.formatNumber(value) }</td>
+                      <td>{ name }</td>
+                      <td>{ Number(ask).toFixed(2) }</td>
+                      <td>{ this.formatNumber(value * ask) }</td>
+                      <td>Real</td>
+                      <td>{}</td>
+                    </tr>
+                  );
+                })
+              : <tr />
+          }
+        </tbody>
       </table>
     );
   }
 }
+
+Table.propTypes = {
+  expenses: propTypes.arrayOf(propTypes.object),
+}.isRequired;
 
 const mapStateToProps = ({ wallet }) => ({
   ...wallet,
