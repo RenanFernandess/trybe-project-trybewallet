293f8404469d8055de940dd058eba5d39f10052f
Renan Fernandes <renzinestuods@gmail.com>
2022-08-29 16:17:08 -0300

    Cria um test que verifica se o botão é ativado apos digitar o e-mail no formato correto e uma senha com no minimo 6 digitos, e outro que verifica se a pagina começa na rota "/"

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouterAndRedux from './helpers/renderWith';
 
@@ -8,6 +9,10 @@ const inputPasswordTestId = 'password-input';
 const buttonText = 'Entrar';
 
 describe('Testes da Pagina Login', () => {
+  it('Verifica se a pagina começa na rota "/"', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    expect(history.location.pathname).toBe('/');
+  });
   it('Verifica se possui um local para a pessoa usuária digitar o Email', () => {
     renderWithRouterAndRedux(<App />);
     const inputEmail = screen.getByTestId(inputEmailTestId);
@@ -23,5 +28,18 @@ describe('Testes da Pagina Login', () => {
     const button = screen.getByRole('button', { name: buttonText });
     expect(button).toBeInTheDocument();
     expect(button).toBeDisabled();
+  });
+  it('Verifica se o botão é ativado apos digitar o e-mail no formato correto e uma senha com no minimo 6 digitos', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const inputPassword = screen.getByTestId(inputPasswordTestId);
+    const inputEmail = screen.getByTestId(inputEmailTestId);
+
+    userEvent.type(inputPassword, '123456');
+    userEvent.type(inputEmail, 'xablau@gmail.com');
+
+    const button = screen.getByRole('button', { name: buttonText });
+
+    expect(button).not.toBeDisabled();
   });
 });
