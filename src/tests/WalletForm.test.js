aa48b064fbe95fc54a57b580c7b12a9206fe88b0
Renan Fernandes <renzinestuods@gmail.com>
2022-08-29 17:34:40 -0300

    Cria um test que verifica se possui um campo para selecionar em qual moeda será registrada a despesa

--- src/tests/WalletForm.test.js
+++ src/tests/WalletForm.test.js
@@ -5,6 +5,25 @@ import Wallet from '../pages/Wallet';
 
 const inputValueTestId = 'value-input';
 const inputDescriptionTestId = 'description-input';
+const inputCurrencyTestId = 'currency-input';
+
+// const currencies = [
+//   'USD',
+//   'CAD',
+//   'GBP',
+//   'ARS',
+//   'BTC',
+//   'LTC',
+//   'EUR',
+//   'JPY',
+//   'CHF',
+//   'AUD',
+//   'CNY',
+//   'ILS',
+//   'ETH',
+//   'XRP',
+//   'DOGE',
+// ];
 
 describe('testa o componente WalletForm', () => {
   it('Verifica se possui um campo para adicionar valor da despesa', () => {
@@ -14,5 +33,12 @@ describe('testa o componente WalletForm', () => {
   it('Verifica se possui um campo para adicionar a descrição da despesa', () => {
     renderWithRouterAndRedux(<Wallet />);
     expect(screen.getByTestId(inputDescriptionTestId)).toBeInTheDocument();
+  });
+  it('Verifica se possui um campo para selecionar em qual moeda será registrada a despesa', () => {
+    renderWithRouterAndRedux(<Wallet />);
+
+    const inputCurrency = screen.getByTestId(inputCurrencyTestId);
+
+    expect(inputCurrency).toBeInTheDocument();
   });
 });
