108605975dba3f57758b60bebb4973c4c76d90dd
Renan Fernandes <renzinestuods@gmail.com>
2022-08-29 20:06:04 -0300

    Cria um test que verifica se possui um campo para selecionar uma categoria (tag) para a despesa

--- src/tests/WalletForm.test.js
+++ src/tests/WalletForm.test.js
@@ -6,6 +6,8 @@ import Wallet from '../pages/Wallet';
 const inputValueTestId = 'value-input';
 const inputDescriptionTestId = 'description-input';
 const inputCurrencyTestId = 'currency-input';
+const inputMethodTestId = 'method-input';
+const inputTagTestId = 'tag-input';
 
 // const currencies = [
 //   'USD',
@@ -36,9 +38,28 @@ describe('testa o componente WalletForm', () => {
   });
   it('Verifica se possui um campo para selecionar em qual moeda será registrada a despesa', () => {
     renderWithRouterAndRedux(<Wallet />);
-
     const inputCurrency = screen.getByTestId(inputCurrencyTestId);
-
     expect(inputCurrency).toBeInTheDocument();
+  });
+  it('Verifica se possui um campo para adicionar qual método de pagamento será utilizado', () => {
+    renderWithRouterAndRedux(<Wallet />);
+    const inputMethod = screen.getByTestId(inputMethodTestId);
+    const [one, two, three] = inputMethod.options;
+    expect(inputMethod).toBeInTheDocument();
+    expect(inputMethod.options).toHaveLength(3);
+    expect(one).toHaveTextContent('Dinheiro');
+    expect(two).toHaveTextContent('Cartão de crédito');
+    expect(three).toHaveTextContent('Cartão de débito');
+  });
+  it('Verifica se possui um campo para selecionar uma categoria (tag) para a despesa', () => {
+    renderWithRouterAndRedux(<Wallet />);
+    const inputTag = screen.getByTestId(inputTagTestId);
+    expect(inputTag).toBeInTheDocument();
+    expect(inputTag.options).toHaveLength(5);
+    expect(inputTag.options[0]).toHaveTextContent('Alimentação');
+    expect(inputTag.options[1]).toHaveTextContent('Lazer');
+    expect(inputTag.options[2]).toHaveTextContent('Trabalho');
+    expect(inputTag.options[3]).toHaveTextContent('Transporte');
+    expect(inputTag.options[4]).toHaveTextContent('Saúde');
   });
 });
