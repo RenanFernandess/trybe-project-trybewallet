807f2afd08e94e63b396e9afd896714c03a48603
Renan Fernandes <renzinestuods@gmail.com>
2022-08-30 17:49:30 -0300

    Verifica se ao clicar no botão de deletar a despesa é apagada

--- src/tests/removeExpense.test.js
+++ src/tests/removeExpense.test.js
@@ -5,7 +5,7 @@ import renderWithRouterAndRedux from './helpers/renderWith';
 import Table from '../components/Table';
 import mockData from './helpers/mockData';
 
-const totalFieldTestId = 'total-field';
+// const totalFieldTestId = 'total-field';
 const deleteButtonTestId = 'delete-btn';
 
 const wallet = {
@@ -46,5 +46,32 @@ describe('Testa se é possivel remover uma despesa', () => {
   it('Verifica se possui um Botão para remover a despesa', () => {
     renderWithRouterAndRedux(<Table />, { initialState: wallet });
     expect(screen.getAllByTestId(deleteButtonTestId)).toHaveLength(3);
+  });
+  it('Verifica se ao clicar no botão de deletar a despesa é apagada', () => {
+    renderWithRouterAndRedux(<Table />, { initialState: wallet });
+
+    expect(screen.getByText(/^Chinelo quadrado$/gi, { selector: 'td' })).toBeInTheDocument();
+    expect(screen.getByText(/^Lazer$/gi, { selector: 'td' })).toBeInTheDocument();
+    expect(screen.getByText(/^40.00$/gi, { selector: 'td' })).toBeInTheDocument();
+    expect(screen.getByText(/^190.12$/gi, { selector: 'td' })).toBeInTheDocument();
+
+    expect(screen.getByText(/^Academia$/gi, { selector: 'td' })).toBeInTheDocument();
+    expect(screen.getByText(/^Saúde$/gi, { selector: 'td' })).toBeInTheDocument();
+    expect(screen.getByText(/^90.00$/gi, { selector: 'td' })).toBeInTheDocument();
+    expect(screen.getByText(/^427.77$/gi, { selector: 'td' })).toBeInTheDocument();
+
+    const [firstButton] = screen.getAllByTestId(deleteButtonTestId);
+
+    userEvent.click(firstButton);
+
+    expect(screen.queryByText(/^Chinelo quadrado$/gi, { selector: 'td' })).not.toBeInTheDocument();
+    expect(screen.queryByText(/^Lazer$/gi, { selector: 'td' })).not.toBeInTheDocument();
+    expect(screen.queryByText(/^40.00$/gi, { selector: 'td' })).not.toBeInTheDocument();
+    expect(screen.queryByText(/^190.12$/gi, { selector: 'td' })).not.toBeInTheDocument();
+
+    expect(screen.getByText(/^Academia$/gi, { selector: 'td' })).toBeInTheDocument();
+    expect(screen.getByText(/^Saúde$/gi, { selector: 'td' })).toBeInTheDocument();
+    expect(screen.getByText(/^90.00$/gi, { selector: 'td' })).toBeInTheDocument();
+    expect(screen.getByText(/^427.77$/gi, { selector: 'td' })).toBeInTheDocument();
   });
 });
