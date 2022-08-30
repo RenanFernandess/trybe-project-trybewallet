cdfce4fdfde5d133eaaf2189ad7fa4943e830194
Renan Fernandes <renzinestuods@gmail.com>
2022-08-30 17:07:53 -0300

    Cria a action expenseDelete

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,10 +2,13 @@ export const REQUEST_API = 'REQUEST_API';
 export const RESPONSE = 'RESPONSE';
 export const USER = 'USER';
 export const WALLET = 'WALLET';
+export const DELETE_EXPENSE = 'DELETE';
 
 export const userAction = (payload) => ({ type: USER, payload });
 
 export const walletAction = (payload) => ({ type: WALLET, payload });
+
+export const expenseDelete = (payload) => ({ type: DELETE_EXPENSE, payload });
 
 const requestAction = () => ({ type: REQUEST_API });
 
