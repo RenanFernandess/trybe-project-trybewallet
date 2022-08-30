b0bea7e0e6abc26aaeb34eb8888a08d7ddca5e2e
Renan Fernandes <renzinestuods@gmail.com>
2022-08-30 14:19:20 -0300

    Verifica se possui um campo para selecionar em qual moeda será registrada a despesa

--- src/tests/WalletForm.test.js
+++ src/tests/WalletForm.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouterAndRedux from './helpers/renderWith';
 import Wallet from '../pages/Wallet';
+import WalletForm from '../components/WalletForm';
 
 const inputValueTestId = 'value-input';
 const inputDescriptionTestId = 'description-input';
@@ -9,23 +10,28 @@ const inputCurrencyTestId = 'currency-input';
 const inputMethodTestId = 'method-input';
 const inputTagTestId = 'tag-input';
 
-// const currencies = [
-//   'USD',
-//   'CAD',
-//   'GBP',
-//   'ARS',
-//   'BTC',
-//   'LTC',
-//   'EUR',
-//   'JPY',
-//   'CHF',
-//   'AUD',
-//   'CNY',
-//   'ILS',
-//   'ETH',
-//   'XRP',
-//   'DOGE',
-// ];
+const wallet = {
+  wallet: {
+    currencies: [
+      'USD',
+      'CAD',
+      'GBP',
+      'ARS',
+      'BTC',
+      'LTC',
+      'EUR',
+      'JPY',
+      'CHF',
+      'AUD',
+      'CNY',
+      'ILS',
+      'ETH',
+      'XRP',
+      'DOGE',
+    ],
+  },
+};
+const { wallet: { currencies } } = wallet;
 
 describe('testa o componente WalletForm', () => {
   it('Verifica se possui um campo para adicionar valor da despesa', () => {
@@ -37,9 +43,25 @@ describe('testa o componente WalletForm', () => {
     expect(screen.getByTestId(inputDescriptionTestId)).toBeInTheDocument();
   });
   it('Verifica se possui um campo para selecionar em qual moeda será registrada a despesa', () => {
-    renderWithRouterAndRedux(<Wallet />);
+    renderWithRouterAndRedux(<WalletForm />, { initialState: wallet });
     const inputCurrency = screen.getByTestId(inputCurrencyTestId);
     expect(inputCurrency).toBeInTheDocument();
+    expect(inputCurrency.options).toHaveLength(currencies.length);
+    expect(inputCurrency.options[0]).toHaveTextContent(currencies[0]);
+    expect(inputCurrency.options[1]).toHaveTextContent(currencies[1]);
+    expect(inputCurrency.options[2]).toHaveTextContent(currencies[2]);
+    expect(inputCurrency.options[3]).toHaveTextContent(currencies[3]);
+    expect(inputCurrency.options[4]).toHaveTextContent(currencies[4]);
+    expect(inputCurrency.options[5]).toHaveTextContent(currencies[5]);
+    expect(inputCurrency.options[6]).toHaveTextContent(currencies[6]);
+    expect(inputCurrency.options[7]).toHaveTextContent(currencies[7]);
+    expect(inputCurrency.options[8]).toHaveTextContent(currencies[8]);
+    expect(inputCurrency.options[9]).toHaveTextContent(currencies[9]);
+    expect(inputCurrency.options[10]).toHaveTextContent(currencies[10]);
+    expect(inputCurrency.options[11]).toHaveTextContent(currencies[11]);
+    expect(inputCurrency.options[12]).toHaveTextContent(currencies[12]);
+    expect(inputCurrency.options[13]).toHaveTextContent(currencies[13]);
+    expect(inputCurrency.options[14]).toHaveTextContent(currencies[14]);
   });
   it('Verifica se possui um campo para adicionar qual método de pagamento será utilizado', () => {
     renderWithRouterAndRedux(<Wallet />);
