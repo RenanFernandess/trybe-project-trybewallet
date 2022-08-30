869c05b6797215928b93490be83899a814a40e8d
Renan Fernandes <renzinestuods@gmail.com>
2022-08-30 10:58:05 -0300

    Cria um test que verifica se o Verifica se o valor total é atualizado

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -2,12 +2,48 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouterAndRedux from './helpers/renderWith';
 import Wallet from '../pages/Wallet';
+import mockData from './helpers/mockData';
+import Header from '../components/Header';
 
 const emailFieldTestId = 'email-field';
 const totalFieldTestId = 'total-field';
 const headerCurrencyFieldTestId = 'header-currency-field';
 const user = { user: { email: 'xablau@gmail.com' } };
 const { user: { email } } = user;
+const expectedValue = '784.26';
+const wallet = {
+  wallet: {
+    expenses: [
+      {
+        id: 0,
+        value: '40',
+        description: 'Chinelo quadrado',
+        currency: 'USD',
+        method: 'Dinheiro',
+        tag: 'Lazer',
+        exchangeRates: mockData,
+      },
+      {
+        id: 1,
+        value: '90',
+        description: 'Academia',
+        currency: 'USD',
+        method: 'Dinheiro',
+        tag: 'Saúde',
+        exchangeRates: mockData,
+      },
+      {
+        id: 2,
+        value: '35',
+        description: 'Hamburgão ',
+        currency: 'USD',
+        method: 'Dinheiro',
+        tag: 'Alimentação',
+        exchangeRates: mockData,
+      },
+    ],
+  },
+};
 
 describe('Testa o componente Header', () => {
   it('Verifica se possui um elemento que exiba o e-mail da pessoa usuária que fez login', () => {
@@ -31,5 +67,11 @@ describe('Testa o componente Header', () => {
     const emailElement = screen.getByTestId(emailFieldTestId);
     expect(emailElement).toBeInTheDocument();
     expect(emailElement).toHaveTextContent(email);
+  });
+  it('Verifica se o Verifica se o valor total é atualizado', () => {
+    renderWithRouterAndRedux(<Header />, { initialState: wallet });
+    const totalField = screen.getByTestId(totalFieldTestId);
+    expect(totalField).toBeInTheDocument();
+    expect(totalField).toHaveTextContent(expectedValue);
   });
 });
