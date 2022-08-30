57dc6562b64f20b9f12f0878ee01138b2fb6f208
Renan Fernandes <renzinestuods@gmail.com>
2022-08-30 17:58:22 -0300

    Verifica se clicar no botão de deletar, o valor total é calculado de novo

--- src/tests/removeExpense.test.js
+++ src/tests/removeExpense.test.js
@@ -4,12 +4,30 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouterAndRedux from './helpers/renderWith';
 import Table from '../components/Table';
 import mockData from './helpers/mockData';
+import Wallet from '../pages/Wallet';
 
-// const totalFieldTestId = 'total-field';
+const totalFieldTestId = 'total-field';
 const deleteButtonTestId = 'delete-btn';
 
 const wallet = {
   wallet: {
+    currencies: [
+      'USD',
+      'CAD',
+      'GBP',
+      'ARS',
+      'BTC',
+      'LTC',
+      'EUR',
+      'JPY',
+      'CHF',
+      'AUD',
+      'CNY',
+      'ILS',
+      'ETH',
+      'XRP',
+      'DOGE',
+    ],
     expenses: [
       {
         id: 0,
@@ -47,7 +65,7 @@ describe('Testa se é possivel remover uma despesa', () => {
     renderWithRouterAndRedux(<Table />, { initialState: wallet });
     expect(screen.getAllByTestId(deleteButtonTestId)).toHaveLength(3);
   });
-  it('Verifica se ao clicar no botão de deletar a despesa é apagada', () => {
+  it('Verifica se clicar no botão de deletar, a despesa é apagada da tabela', () => {
     renderWithRouterAndRedux(<Table />, { initialState: wallet });
 
     expect(screen.getByText(/^Chinelo quadrado$/gi, { selector: 'td' })).toBeInTheDocument();
@@ -61,7 +79,6 @@ describe('Testa se é possivel remover uma despesa', () => {
     expect(screen.getByText(/^427.77$/gi, { selector: 'td' })).toBeInTheDocument();
 
     const [firstButton] = screen.getAllByTestId(deleteButtonTestId);
-
     userEvent.click(firstButton);
 
     expect(screen.queryByText(/^Chinelo quadrado$/gi, { selector: 'td' })).not.toBeInTheDocument();
@@ -73,5 +90,14 @@ describe('Testa se é possivel remover uma despesa', () => {
     expect(screen.getByText(/^Saúde$/gi, { selector: 'td' })).toBeInTheDocument();
     expect(screen.getByText(/^90.00$/gi, { selector: 'td' })).toBeInTheDocument();
     expect(screen.getByText(/^427.77$/gi, { selector: 'td' })).toBeInTheDocument();
+  });
+  it('Verifica se clicar no botão de deletar, o valor total é calculado de novo', () => {
+    renderWithRouterAndRedux(<Wallet />, { initialState: wallet });
+    expect(screen.getByTestId(totalFieldTestId)).toHaveTextContent(/^784.26$/g);
+
+    const [firstButton] = screen.getAllByTestId(deleteButtonTestId);
+    userEvent.click(firstButton);
+
+    expect(screen.getByTestId(totalFieldTestId)).toHaveTextContent(/^594.13$/g);
   });
 });
